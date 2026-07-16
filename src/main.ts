5b6116380337b2a471620840dc4cc9dfd7782b61
Christian Langelaar <christian.langelaar@crv4all.com>
2026-07-16 15:46:16 +0200

    Restore privacy-safe analytics scope

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
-import { captureError } from './services/posthogAnalytics'
 
-const app = createApp(App)
-
-app.config.errorHandler = (err) => {
-  captureError(err)
-}
-
-app.mount('#app')
+createApp(App).mount('#app')
